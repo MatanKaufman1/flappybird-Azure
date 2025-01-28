8806075e24765cf324b7cad82146c947c2b321af
matan.kaufman <matankaufman1@gmail.com>
2025-01-28 13:40:02 +0200

    update files

--- terraform/acr/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "acr_name" {}
-variable "subnet_id" {}
--- terraform/aks/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
-}
-
-output "aks_cluster_id" {
-  value = azurerm_kubernetes_cluster.aks_cluster.id
-}
-
